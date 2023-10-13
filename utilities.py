85c3a4cfa51b65e00758c4fbc46289c44bae2a30
Mathis Maquenne <mathismaquenne@gmail.com>
2023-10-13 17:01:04 +0200

    feat(datasets): add a function to load csv datasets

--- utilities.py
+++ utilities.py
@@ -1,13 +1,52 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 
+def _detect_csv_delimiter(path):
+    delimiters = [",", "\t", ";", "|"]
+
+    with open(path, "r", encoding="utf-8") as file:
+        first_line = file.readline()
+        for delimiter in delimiters:
+            if delimiter in first_line:
+                return delimiter
+
+def load_dataset(path, target, delimiter=None):
+    if delimiter is None:
+        delimiter = _detect_csv_delimiter(path)
+
+        if delimiter is None:
+            raise Exception("Could not detect the delimiter of the CSV file. Please specify it manually")
+    try:
+        dataset = pd.read_csv(path, delimiter=delimiter)
+        dataset = dataset.dropna()
+        dataset = dataset.reset_index(drop=True)
+
+        categorical_columns = dataset.select_dtypes(include=["object", "category"]).columns
+
+        for column in categorical_columns:
+            codes, _ = pd.factorize(dataset[column])
+            dataset[column] = codes
+
+            if len(dataset[column].unique()) == len(dataset[column]):
+                dataset = dataset.drop(column, axis=1)
+
+        X = dataset.drop(target, axis=1).values
+        y = dataset[target].values.reshape(-1, 1)
+
+        return X, y
+    except FileNotFoundError:
+        raise Exception("The specified file does not exist")
+    except KeyError:
+        raise Exception("The specified target column does not exist")
+    
 def normalize_dataset(X):
     normalized_dataset = (X - np.min(X)) / (np.max(X) - np.min(X))
     
     return normalized_dataset
-
+    
 def display_dataset(X, y):
     if X.shape[1] == 2:
         unique_classes = np.unique(y)
